33d4f84ec1dc4ddecf21f8a53de61b4fdb94fc20
Vitesh Bava <vitesh.bava@gmail.com>
2021-12-04 21:27:06 +1300

    shadow on delete button

--- src/components/TransactionList/TransactionItem/TransactionItem.js
+++ src/components/TransactionList/TransactionItem/TransactionItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../../UI/Card/Card";
+import Button from "../../UI/Button/Button";
 import { Icon } from "@iconify/react";
 import styles from "./TransactionItem.module.css";
 
@@ -17,12 +18,12 @@ export default function TransactionItem({ item, readOnly }) {
           <span>{description}</span>
           <span>{amount}</span>
         </Card>
-        <Card className={`${styles.action} ${styles["action--edit"]}`}>
+        <Button className={`${styles.action} ${styles["action--edit"]}`}>
           <Icon icon="entypo:pencil" />
-        </Card>
-        <Card className={`${styles.action} ${styles["action--delete"]}`}>
+        </Button>
+        <Button className={`${styles.action} ${styles["action--delete"]}`}>
           <Icon icon="bi:trash-fill" />
-        </Card>
+        </Button>
       </div>
     </div>
   );
