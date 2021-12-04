ca562bb1753e8df2351114f90294afa88d747dc1
Vitesh Bava <vitesh.bava@gmail.com>
2021-12-04 21:05:24 +1300

    done slide

--- src/components/TransactionList/TransactionItem/TransactionItem.js
+++ src/components/TransactionList/TransactionItem/TransactionItem.js
@@ -7,21 +7,23 @@ export default function TransactionItem({ item }) {
   const { description } = item;
   const amount = (Math.round(item.amount * 100) / 100).toFixed(2);
   return (
-    <div className={styles.wrapper}>
-      <Card
-        className={`${styles.item} ${
-          amount < 0 ? styles["item--expense"] : styles["item--income"]
-        }`}
-      >
-        <span>{description}</span>
-        <span>{amount}</span>
-      </Card>
-      <Card className={`${styles.action} ${styles["action--edit"]}`}>
-        <Icon icon="entypo:pencil" />
-      </Card>
-      <Card className={`${styles.action} ${styles["action--delete"]}`}>
-        <Icon icon="bi:trash-fill" />
-      </Card>
+    <div className={styles["relative-wrapper"]}>
+      <div className={styles["absolute-wrapper"]}>
+        <Card
+          className={`${styles.item} ${
+            amount < 0 ? styles["item--expense"] : styles["item--income"]
+          }`}
+        >
+          <span>{description}</span>
+          <span>{amount}</span>
+        </Card>
+        <Card className={`${styles.action} ${styles["action--edit"]}`}>
+          <Icon icon="entypo:pencil" />
+        </Card>
+        <Card className={`${styles.action} ${styles["action--delete"]}`}>
+          <Icon icon="bi:trash-fill" />
+        </Card>
+      </div>
     </div>
   );
 }
